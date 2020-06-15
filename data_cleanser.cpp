6aaa4fbe555514e03289a53cc8b19b53b038375d
avirajBevli <45387375+avirajBevli@users.noreply.github.com>
2020-06-15 13:57:20 +0530

    Create data_cleanser.cpp
    Cleans the text file downloaded from the TSPLIB page to change it into a format usable by main

---
+++ data_cleanser.cpp
@@ -0,0 +1,35 @@
+#include<fstream>
+#include<stdlib.h> 
+#include<stdio.h>
+#include<iostream>
+#include<time.h> 
+#include<cmath>
+#include <stdio.h> 
+#include <stdlib.h> 
+
+using namespace std;
+
+int main()
+{
+  srand(time(0)); 
+  ofstream city_coord_list;
+  city_coord_list.open("city_coord_data.txt");
+
+  int index;
+  double x, y;
+  ifstream fin; 
+  //fin.open("bier127.txt");
+  fin.open("kroA100.txt");
+  
+  fin>>index;
+  while(!fin.eof())
+  {
+    fin>>x;
+    fin>>y;
+    city_coord_list<<x<<" "<<y<<endl;
+    fin>>index;
+  }
+  std::cout<<"Cleaned the data"<<endl;
+  city_coord_list.close();
+  return 0;
+}
